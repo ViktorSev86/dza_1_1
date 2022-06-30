935f07e6927f2dd1c1dc4c74e549facbf50ec379
ViktorSev86 <butioxa_86@mail.ru>
2022-06-30 23:03:02 +0300

    Edittext

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,12 +1,23 @@
 package ru.netology.nmedia.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
+
+    val data: MutableLiveData<List<Post>>
+
     fun getAll(): LiveData<List<Post>>
     fun likeById(id: Long)
     fun shareById(id: Long)
     fun glazById(id: Long)
     fun delete(postId: Long)
+    fun save(post: Post)
+
+    companion object {
+        const val NEW_POST_ID = 0L
+    }
+
+
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -7,98 +7,28 @@ import androidx.lifecycle.MutableLiveData
 
 class PostRepositoryInMemoryImpl : PostRepository {
 
-    private var posts = listOf(
-        Post(
-            id = 9,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Освоение новой профессии — это не только открывающиеся возможности и перспективы, но и настоящий вызов самому себе. Приходится выходить из зоны комфорта и перестраивать привычный образ жизни: менять распорядок дня, искать время для занятий, быть готовым к возможным неудачам в начале пути. В блоге рассказали, как избежать стресса на курсах профпереподготовки → http://netolo.gy/fPD",
-            published = "23 сентября в 10:12",
-            likedByMe = false,
-            countLikes = 999u,
-            countShare = 99u,
-            countGlaz = 999999u
-        ),
-        Post(
-            id = 8,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Делиться впечатлениями о любимых фильмах легко, а что если рассказать так, чтобы все заскучали \uD83D\uDE34\n",
-            published = "22 сентября в 10:14",
-            likedByMe = false,
-            countLikes = 0u,
-            countShare = 0u,
-            countGlaz = 0u
-        ),
-        Post(
-            id = 7,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Таймбоксинг — отличный способ навести порядок в своём календаре и разобраться с делами, которые долго откладывали на потом. Его главный принцип — на каждое дело заранее выделяется определённый отрезок времени. В это время вы работаете только над одной задачей, не переключаясь на другие. Собрали советы, которые помогут внедрить таймбоксинг \uD83D\uDC47\uD83C\uDFFB",
-            published = "22 сентября в 10:12",
-            likedByMe = false,
-            countLikes = 99u,
-            countShare = 999u,
-            countGlaz = 100u
-        ),
-        Post(
-            id = 6,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "\uD83D\uDE80 24 сентября стартует новый поток бесплатного курса «Диджитал-старт: первый шаг к востребованной профессии» — за две недели вы попробуете себя в разных профессиях и определите, что подходит именно вам → http://netolo.gy/fQ",
-            published = "21 сентября в 10:12",
-            likedByMe = false,
-            countLikes = 999u,
-            countShare = 99u,
-            countGlaz = 999999u
-        ),
-        Post(
-            id = 5,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Диджитал давно стал частью нашей жизни: мы общаемся в социальных сетях и мессенджерах, заказываем еду, такси и оплачиваем счета через приложения.",
-            published = "20 сентября в 10:14",
-            likedByMe = false,
-            countLikes = 999u,
-            countShare = 99u,
-            countGlaz = 999999u
-        ),
-        Post(
-            id = 4,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Большая афиша мероприятий осени: конференции, выставки и хакатоны для жителей Москвы, Ульяновска и Новосибирска \uD83D\uDE09",
-            published = "19 сентября в 14:12",
-            likedByMe = false,
-            countLikes = 999u,
-            countShare = 99u,
-            countGlaz = 999999u
-        ),
-        Post(
-            id = 3,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Языков программирования много, и выбрать какой-то один бывает нелегко. Собрали подборку статей, которая поможет вам начать, если вы остановили свой выбор на JavaScript.",
-            published = "19 сентября в 10:24",
-            likedByMe = false,
-            countLikes = 999u,
-            countShare = 99u,
-            countGlaz = 999999u
-        ),
-        Post(
-            id = 2,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Знаний хватит на всех: на следующей неделе разбираемся с разработкой мобильных приложений, учимся рассказывать истории и составлять PR-стратегию прямо на бесплатных занятиях \uD83D\uDC47",
-            published = "18 сентября в 10:12",
-            likedByMe = false,
-            countLikes = 999u,
-            countShare = 99u,
-            countGlaz = 999999u
-        ),
-        Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36",
-            likedByMe = false,
-            countLikes = 999u,
-            countShare = 99u,
-            countGlaz = 999999u
-        ),
-    )
+    private var nextId = GENERATED_POST_AMOUNT.toLong()
+
+    private val posts
+    get() = checkNotNull(data.value) {
+        "Data value should not be null"
+    }
+
+    override val data = MutableLiveData(
+        List(GENERATED_POST_AMOUNT) {index ->
+            Post(
+                id = index + 1L,
+                author = "Нетология. Университет интернет-профессий будущего",
+                content = "Some random content $index",
+                published = "30.06.2022",
+                likedByMe = false,
+                countLikes = 0u,
+                countShare = 0u,
+                countGlaz = 0u
+            )
+        })
+
+
 
 /*    private var post = Post(
         id = 1L,
@@ -113,12 +43,10 @@ class PostRepositoryInMemoryImpl : PostRepository {
 
  */
 
-    private val data = MutableLiveData(posts)
-
     override fun getAll(): LiveData<List<Post>> = data
 
     override fun likeById(id: Long) {
-        posts = posts.map {
+        data.value = posts.map {
             if (it.id != id) it
             else it.copy(likedByMe = !it.likedByMe,
                 countLikes = if (!it.likedByMe) it.countLikes + 1u else it.countLikes - 1u)
@@ -127,7 +55,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
     }
 
     override fun shareById(id: Long) {
-        posts = posts.map {
+        data.value = posts.map {
             if (it.id != id) it
             else it.copy(countShare = it.countShare + 1u)
         }
@@ -135,7 +63,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
     }
 
     override fun glazById(id: Long) {
-        posts = posts.map {
+        data.value = posts.map {
             if (it.id != id) it
             else it.copy(countGlaz = it.countGlaz + 1u)
         }
@@ -143,10 +71,35 @@ class PostRepositoryInMemoryImpl : PostRepository {
     }
 
     override fun delete(postId: Long) {
-        posts = posts.filter {
+        data.value = posts.filter {
             it.id != postId
         }
         data.value = posts
+    }
+
+    override fun save(post: Post) {
+        if (post.id == PostRepository.NEW_POST_ID) {
+            insert(post)
+        } else {
+            update(post)
+        }
+
+    }
+
+    private fun update(post: Post) {
+        data.value = posts.map {
+            if (it.id == post.id) post else it
+        }
+
+    }
+
+    private fun insert(post: Post) {
+        data.value = posts + post
+
+    }
+
+    private companion object {
+        const val GENERATED_POST_AMOUNT = 1000
     }
 
     /*   override fun like() {
