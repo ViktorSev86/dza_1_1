c695f4d8057bb9e802e06919f02d83dc796419f0
ViktorSev86 <butioxa_86@mail.ru>
2022-07-05 22:25:28 +0300

    dz31begin

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,10 +1,14 @@
 package ru.netology.nmedia.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.view.*
+import ru.netology.nmedia.R
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -14,56 +18,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter (
-            onLikeClicked = {post ->  viewModel.likeById(post.id)},
-            onShareClicked = {post ->  viewModel.shareById(post.id)},
-            onGlazClicked = {post ->  viewModel.glazById(post.id)}
-        )
+        val adapter = PostsAdapter (viewModel)
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.list = posts
         }
 
-        /*
-        fun UInt.uIntToString():String {  // Переделать расширением типа
-            val res = when {
-                this < 1000u -> this.toString()
-                this < 10000u -> "${this/1000u}.${this/100u%10u}K"
-                this < 1000000u -> "${this/1000u}K"
-                this < 10000000u -> "${this/1000000u}.${this/100000u%10u}M"
-                this < 100000000u -> "${this/1000000u}M"
-                else -> "много"
+        viewModel.edited.observe(this) { post ->
+            if (post.id == 0L) {
+                return@observe
             }
-            return res
-        }
-
-        val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                avatar.setImageResource(R.drawable.ic_channel_foreground)
-                authorName.text = post.author
-                date.text = post.published
-                text.text = post.content
-                countLikes.text = post.countLikes.uIntToString()
-                countShare.text = post.countShare.uIntToString()
-                countGlaz.text = post.countGlaz.uIntToString()
-                likes.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_liked_favorite_24 else R.drawable.ic_baseline_favorite_border_24
-                )
-
-                likes.setOnClickListener {
-                    viewModel.like()
-
-                }
-                share.setOnClickListener {
-                    viewModel.share()
-                }
-
-                glaz.setOnClickListener {
-                    viewModel.glaz()
-                }
+            with(binding.content) {
+                requestFocus()
+                setText(post.content)
             }
         }
-        */
+
+        binding.save.setOnClickListener {
+            with(binding.content) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        binding.content.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                val t = text.toString()
+
+                viewModel.changeContent(t)
+                viewModel.save(content = t)
+
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
+        }
+
+        binding.cansel.setOnClickListener {
+            with(binding.content) {
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
+        }
     }
 }
